16a374349078e7737f6743ce12ebdba3a40ee60d
Alice <a.j.stack@sbcglobal.net>
2018-05-10 13:29:59 -0500

    wrote play/pause click handler but it won't work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -61,23 +61,13 @@ function toggleSubmitButton(){
 //calling function
 toggleSubmitButton();
 
-//click handler to push topics to array
-$("#addCharacter").on("click", function(event) {
-
-    event.preventDefault();
-    var topic = $("#characterInput").val().trim();
-    topics.push(topic);
-    //calling function to create new buttons   
-    createButtons();
-    
-  });
 
 //click function
 $("button").on("click", function(){
     var apiKey = "7nmB5rC887rydxETvwZs3l1ZDEJfK6eG";
     //accessing the correct character from attribute
     var character = $(this).attr("data-topic"); 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=" + apiKey + "&limit=10"; 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=" + apiKey + "&limit=1"; 
 
     //ajax call
     $.ajax({
@@ -94,12 +84,18 @@ $("button").on("click", function(){
         //accessing the correct gifs
         for (var j = 0; j < results.length; j++) {
             var imageURL = results[j].images.downsized_still.url;
+            var imageURLAnimate = results[j].images.downsized_large.url;
             console.log(imageURL);
             var characterImage = $("<img>");
 
             //setting attributes to dispaly gif
             characterImage.attr("src", imageURL);
             characterImage.attr("alt", "character image");
+            characterImage.attr("data-still", imageURL);
+            characterImage.attr("data-animate", imageURLAnimate);
+            console.log(imageURLAnimate);
+            characterImage.attr("data-state", "still");
+            characterImage.addClass("gif");
 
             $("#characters").prepend(characterImage);
         }
@@ -107,10 +103,30 @@ $("button").on("click", function(){
 
 });
 
+//click handler to push topics to array
+$("#addCharacter").on("click", function(event) {
+
+    event.preventDefault();
+    var topic = $("#characterInput").val().trim();
+    topics.push(topic);
+    //calling function to create new buttons   
+    createButtons();
+    
+  });
+
+$(".gif").on("click", function(){ //{not working}
+    var state = $(this).attr("data-state");
+
+    if(state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
 
 
-
-
-//figure out WHY NEW BUTTONS WON'T WORK
-//click handler for the pause/play functionality
+//figure out WHY NOTHING WORKS AFTER I ADD A NEW BUTTON
+//click handler for the pause/play functionality {not working}
 //finish commenting rest of code
